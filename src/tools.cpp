b81b3c14ae080bcce4ecc972117d55ac3fce4688
vikramriyer <vikram.iyer09@gmail.com>
2018-12-30 15:04:31 +0530

    code: minor bugs in rmse and jacobian calculation fixed. Reduced RMSE to 0.09, 0.08, 0.45, 0.43 :: The max threshold is 0.11, 0.11, 0.52, 0.52

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,5 +1,5 @@
-#include "tools.h"
 #include <iostream>
+#include "tools.h"
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
@@ -11,68 +11,72 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
+  /**
+    * Calculate the RMSE here.
+  */
 
-	VectorXd rmse(4);
-	rmse << 0,0,0,0;
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
 
-	// TODO: YOUR CODE HERE
-	// check the validity of the following inputs:
-	//  * the estimation vector size should not be zero
-	//  * the estimation vector size should equal ground truth vector size
-	if (estimations.size() != ground_truth.size()
-			|| estimations.size() == 0){
-			std::cout << "Better check your ground truth calculations mate!" << std::endl;
-			return rmse;
-	}
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+    std::cout << "Invalid estimation or ground_truth data" << std::endl;
+    return rmse;
+  }
 
-	// TODO: accumulate squared residuals
-	for (int i=0; i < estimations.size(); ++i) {
-		VectorXd residual = estimations[i] - ground_truth[i];
+  //accumulate squared residuals
+  for(unsigned int i=0; i < estimations.size(); ++i){
 
-		//coefficient-wise multiplication
-		residual = residual.array() * residual.array();
-		rmse += residual;
-	}
+    VectorXd residual = estimations[i] - ground_truth[i];
 
-	// TODO: calculate the mean
-	rmse /= estimations.size();
+    //coefficient-wise multiplication
+    residual = residual.array()*residual.array();
+    rmse += residual;
+  }
 
+  //calculate the mean
+  rmse = rmse/estimations.size();
 
-	// TODO: calculate the squared root
-	rmse = rmse.array().sqrt();
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
 
-	// return the result
-	return rmse;
+  //return the result
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+    * Calculate a Jacobian here.
+  */
+  MatrixXd Hj(3,4);
+  Hj << 0,0,0,0,
+        0,0,0,0,
+        0,0,0,0;
 
-	MatrixXd Hj(3,4);
-	Hj << 0,0,0,0,
-		  0,0,0,0,
-		  0,0,0,0;
-	// recover state parameters
-	float px = x_state(0);
-	float py = x_state(1);
-	float vx = x_state(2);
-	float vy = x_state(3);
+  //recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
 
-	// TODO: YOUR CODE HERE
-	float c1 = px*px+py*py; //std::max(1.0e-8f, px*px+py*py);
-	float c2 = sqrt(c1); //std::max(1.0e-8, sqrt(c1));
-	float c3 = (c1*c2); //std::max(1.0e-8f, (c1*c2));
+  //pre-compute a set of terms to avoid repeated calculation
+  float c1 = px*px+py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
 
-	// check division by zero
-	if (fabs(c1) < 0.0001 || (fabs(c2) < .0001) || (fabs(c3) < .0001)){
-		std::cout << "Error: Division by Zero" << std::endl;
-		return Hj;
-	}
+  //check division by zero
+  if(fabs(c1) < 0.0001){
+    std::cout << "Function CalculateJacobian() has Error: Division by Zero" << std::endl;
+    return Hj;
+  }
 
-	// compute the Jacobian matrix
-	//compute the Jacobian matrix
-	Hj << (px/c2), (py/c2), 0, 0,
-		  (py/c1), (px/c1), 0, 0,
-	   	   py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+  //compute the Jacobian matrix
+  Hj << (px/c2),                (py/c2),                0,      0,
+        -(py/c1),               (px/c1),                0,      0, 
+        py*(vx*py - vy*px)/c3,  px*(px*vy - py*vx)/c3,  px/c2,  py/c2;
 
-	return Hj;
+  return Hj;
+
 }
